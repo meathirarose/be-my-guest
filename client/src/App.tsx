f40a24d646f372ee67aa086b214d751242ec29d7
meathirarose <athirarosejohn@gmail.com>
2024-12-18 11:38:35 +0530

    feat(client): implement design for signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage"; 
 import LandingPage from "./pages/LandingPage";   
+import SignUpPage from "./pages/SignUpPage";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
